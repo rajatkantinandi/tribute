0611cb2ff021d45e62d7af75c5e01c0fad490aa1
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2021-03-26 17:39:05 +0530

    Fix: need to hide menu when mentionText.length < menuShowMinLength - Fix: show menu when backspace in pressed & menu is hidden currently

--- example/tribute.js
+++ example/tribute.js
@@ -257,10 +257,11 @@
         }
 
         if (instance.tribute.current.mentionText.length < instance.tribute.current.collection.menuShowMinLength) {
+          instance.tribute.hideMenu();
           return;
         }
 
-        if ((instance.tribute.current.trigger || instance.tribute.autocompleteMode) && instance.commandEvent === false || instance.tribute.isActive && event.keyCode === 8) {
+        if ((instance.tribute.current.trigger || instance.tribute.autocompleteMode) && instance.commandEvent === false || instance.tribute.isActive || event.keyCode === 8) {
           instance.tribute.showMenuFor(this, true);
         }
       }
